2ad85768e13863b9dd1b7f7cf7410a4d167bc637
Rishank Kumar <rishankdhiman072006@gmail.com>
2026-02-08 16:50:31 +0530

    created dot env file and added db passwords

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
 import http from 'node:http';
-import { getExpense } from './controllers/getExpense.js';
-import 
+import getExpense from './controllers/getExpense.js';
 
 const server = http.createServer(function(req , res){
-    res.setHeader('Access-Control-Allow-Origin' , '*');
+    res.setHeader('Access-Control-Allow-Origin' , 'http://localhost:3000');
 
+    try{
     if(req.method === 'OPTIONS'){
-        res.write('server is working fine options recieved');
-        res.end();
+        res.writeHead(200 , {
+            'Content-Type' : 'text/plain',
+            'Timeout' : '10',
+            'cache-control' : 'no-cache'
+        })
+        res.end('This data is not for user')
         return;
     }
     else if(req.method === 'POST'){
-        res.write('ok done');
-        res.end();
+        res.statusCode = 201;
+        res.setHeader('Content-Type' , 'text')
+        res.end('New user is created');
         return;
     }
     else if(req.method === 'GET'){
-        res.write('you got a chips packet');
         res.end();
+        return;
+    }
+    else if(req.method === 'DELETE'){
+        res.end();
+        return;
+    }
+    else if(req.method === 'PUT'){
+        res.end();
+        return;
+    }}
+    catch(err){
+        res.statusCode = 500;
+        res.end()
         return;
     }
 })
