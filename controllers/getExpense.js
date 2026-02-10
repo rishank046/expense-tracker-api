22e3a8168d1755965d38322705362cee5a008faf
Rishank Kumar <rishankdhiman072006@gmail.com>
2026-02-10 22:04:29 +0530

    add logic to get expense of a user by email and password

--- controllers/getExpense.js
+++ controllers/getExpense.js
@@ -1,3 +1,16 @@
-export default function getExpenses(userId , password){
+import db from '../db/database.js';
+import 'dotenv/config';
 
+const addExp = async (email , password) => {
+    try{
+        const [user] = await db.query(`SELECT * FROM ${process.env.USER_TABLE_NAME} WHERE usr_email = ? AND usr_pswd = ?`,[email,password]);
+            const userId = user[0].usr_id;
+            const [expenses] = await db.query(`SELECT * FROM ${process.env.EXPENSE_TABLE_NAME} WHERE usr_id = ?` , userId);
+            return expenses;
+    }catch (error) {
+        console.log(`cannot able to fetch expense data ${error}`);
+        return 1;
+    }
 }
+
+export default addExp;
